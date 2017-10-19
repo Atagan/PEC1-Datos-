c927aff62fe3e3d40142ef4befea157017014fb5
Atagan <luis.ramosm@edu.uah.es>
2017-10-19 22:34:16 +0200

    Creados métodos básicos de la clase

--- PEC1 Datos/PEC1 Datos/NodoCola.h
+++ PEC1 Datos/PEC1 Datos/NodoCola.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "Cliente.h"
+
+class nodo
+{
+public://atributos
+	Cliente infoCliente;
+	nodo *siguiente;//puntero al siguiente elemento de tipo nodoCola	
+
+public:
+	nodo();
+	nodo(Cliente v);
+	~nodo();
+	vehiculo getCliente();//te da la informacion del Cliente
+
+};
